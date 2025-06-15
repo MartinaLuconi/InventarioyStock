2c9acce71bcbb74b5ecee0ada8cd12d379c71954
Belen <patriciasdepartamento14@gmail.com>
2025-06-15 17:15:46 -0300

    funcion en venta service necesita agregar ese atributo para ´asignar el proveedor determinado

--- InventarioyStock/src/main/java/com/IntegradorIO/InventarioyStock/OrdenCompra/DTO/DTODetalleOC.java
+++ InventarioyStock/src/main/java/com/IntegradorIO/InventarioyStock/OrdenCompra/DTO/DTODetalleOC.java
@@ -11,5 +11,7 @@ public class DTODetalleOC {
     private int cantidadArticulo; //tamaño del lote
     private String nombreArticulo;
     private  int codArticulo;
+    private int idProveedor;  // esto lo agregue para asignarle el valor del proveedor determinado al realizar la venta , el dato lo saco de venta service
+
     //private  float precioUnitarioArticulo;
 }
